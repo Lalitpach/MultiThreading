cc4e9e16f3139464ff7ce81889dc135637382867
Lalitpach <lalitpachpande111@gmail.com>
2024-11-24 20:50:27 +0800

    Add project files.

---
+++ MultiThreading/lockKeyword.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+public class LockKeywordDemo
+{
+    public void M1()
+    {
+        lock (this)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Console.WriteLine(i);
+                // Thread.Sleep(2000);
+            }
+        }
+    }
+}
+
+public class Program
+{
+    //static void Main(string[] args)
+    //{
+    //    Test test = new Test();
+
+    //    Thread t1 = new Thread(new ThreadStart(test.M1));
+    //    Thread t2 = new Thread(new ThreadStart(test.M1));
+
+    //    t1.Name = "T1";
+    //    t2.Name = "T2";
+
+    //    t1.Start();
+    //    t2.Start();
+    //}
+}
+}
